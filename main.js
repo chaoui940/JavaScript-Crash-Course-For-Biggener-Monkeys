124e3e09c71a1fa97e8a6e78d3bad8d9f524732f
chaoui940 <daging129@gmail.com>
2023-05-26 18:12:40 +0200

    commit

--- main.js
+++ main.js
@@ -1,5 +1,10 @@
-
-console.log('Hello World')
-let age=27 
-const Name=8000
-console.log(Name)
+console.log("Hello World");
+let age = 27;
+const Name = 8000;
+console.log(Name);
+const person= {
+    firstName:'Baku',
+    LastName : ' Riche',
+    Age : '27',
+}
+console.log(person.firstName+person.LastName)

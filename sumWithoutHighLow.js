fceb7fc88fe856db19708e602667f8f887b37101
chaoui940 <daging129@gmail.com>
2023-11-13 15:20:35 +0100

    a

---
+++ sumWithoutHighLow.js
@@ -0,0 +1,9 @@
+function SumWithoutHigh (array) {
+    if (array == null) return 0 ;
+     let maxValue = Math.max(...array);
+  let minValue = Math.min(...array);
+ let filteredArray = array.filter((x) => x !== maxValue && x !== minValue);
+ let newArray = filteredArray.reduce((acc, current) => acc + current, 0);
+ return newArray;
+}
+SumWithoutHigh([5 ,10 ,6 ,3, 20 , 8]);

a8dc1736cd4bea61995bb7d94018d01bb720c52a
chaoui940 <daging129@gmail.com>
2023-11-14 18:12:57 +0100

    a

--- sumWithoutHighLow.js
+++ sumWithoutHighLow.js
@@ -1,9 +1,9 @@
 function SumWithoutHigh (array) {
     if (array == null) return 0 ;
-     let maxValue = Math.max(...array);
-  let minValue = Math.min(...array);
+let maxValue = Math.max(...array);
+ let minValue = Math.min(...array);
  let filteredArray = array.filter((x) => x !== maxValue && x !== minValue);
  let newArray = filteredArray.reduce((acc, current) => acc + current, 0);
  return newArray;
 }
-SumWithoutHigh([5 ,10 ,6 ,3, 20 , 8]);
+console.log(SumWithoutHigh([5 ,10 ,6 ,3, 20 , 8]));
